6ec41304d223c896848fd67ed8e7764642aa3630
root <gjoseph@frendzi.com>
2022-12-17 13:31:47 +0530

    devlop_1

--- dist/config/i18n/index.js
+++ dist/config/i18n/index.js
@@ -4,12 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const i18n_1 = __importDefault(require("i18n"));
-const path_1 = __importDefault(require("path"));
 const config_1 = __importDefault(require("../config"));
 i18n_1.default.configure({
     locales: ['en_US', 'es_ES', 'joi_en_US', 'joi_es_ES', 'joi_hi_IN'],
     defaultLocale: config_1.default.localizationLanguage,
-    directory: path_1.default.resolve('./assets/locales/', 'i18n')
+    // directory: path.resolve('./assets/locales/', 'i18n')
 });
 exports.default = i18n_1.default;
 //# sourceMappingURL=index.js.map
--- src/config/i18n/index.ts
+++ src/config/i18n/index.ts
@@ -5,7 +5,7 @@ import config from '../config';
 i18n.configure({
     locales: ['en_US', 'es_ES', 'joi_en_US', 'joi_es_ES', 'joi_hi_IN'],
     defaultLocale: config.localizationLanguage,
-    directory: path.resolve('./assets/locales/', 'i18n')
+    // directory: path.resolve('./assets/locales/', 'i18n')
 });
 
 export default i18n;
